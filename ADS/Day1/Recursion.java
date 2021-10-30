41933ca712185aa9523759511cb987cbc8890115
Kiranwaghmare123 <72081819+Kiranwaghmare123@users.noreply.github.com>
2021-10-30 14:03:56 +0530

    Add files via upload

---
+++ ADS/Day1/Recursion.java
@@ -0,0 +1,13 @@
+class Recursion
+{
+	static void show()
+	{
+		System.out.println("Hi Girls !!!!");
+		show();
+	}
+
+	public static void main(String args[])
+	{
+		show();
+	}
+}
---
+++ ADS/Day1/Recursion1.java
@@ -0,0 +1,18 @@
+class Recursion1
+{
+	static int i=0;
+	static void show()
+	{
+		++i;
+		if(i<=5)
+		{
+		System.out.println("Hi Girls !!!!"+i);
+		show();
+		}
+	}
+
+	public static void main(String args[])
+	{
+		show();
+	}
+}
